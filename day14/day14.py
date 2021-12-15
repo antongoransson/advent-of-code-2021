a23f01dccbd2cf61d9530dffc0d1433e96e436f8
antongoransson <anton.goransson95@gmail.com>
2021-12-15 07:29:44 +0100

    Fix day14 off by one error

--- day14/day14.py
+++ day14/day14.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import sys
 
-def solve(n_steps, rules, pairs):
+def solve(template, n_steps, rules, pairs):
     for t in range(n_steps):
         new_pairs = Counter()
         for p, c in pairs.items():
@@ -9,19 +9,19 @@ def solve(n_steps, rules, pairs):
             new_pairs[rules[p] + p[1]] += c
         pairs = new_pairs
     char_count = Counter()
+    char_count[template[0]] += 1
     for (f, s), c in pairs.items():
-        char_count[f] += c
         char_count[s] += c
     max_c, min_c = char_count.most_common()[0], char_count.most_common()[-1]
-    return (max_c[1] - min_c[1]) // 2 + 1
+    return max_c[1] - min_c[1]
 
 
-def solve_part_1(rules, pairs):
-    return solve(10, rules, pairs)
+def solve_part_1(template, rules, pairs):
+    return solve(template, 10, rules, pairs)
 
 
-def solve_part_2(rules, pairs):
-    return solve(40, rules, pairs)
+def solve_part_2(template, rules, pairs):
+    return solve(template, 40, rules, pairs)
 
 def main():
     in_f = sys.argv[1] if len(sys.argv) == 2 else 'in.txt'
@@ -35,9 +35,9 @@ def main():
             rules[p.strip()] = c.strip()
         for i in range(1, len(template)):
             pairs[template[i - 1] + template[i]] += 1
-    sol1 = solve_part_1(rules, pairs)
+    sol1 = solve_part_1(template, rules, pairs)
     print(f'Part 1: {sol1}')
-    sol2 = solve_part_2(rules, pairs)
+    sol2 = solve_part_2(template, rules, pairs)
     print(f'Part 2: {sol2}')
 
 
